25b1da454ae1becae880fe1c9c4fa19439172bbb
Steve Tong <stevetong53@yahoo.com>
2021-02-08 22:20:17 -0800

    Info not with the right person, and cant add image becasue it would mess up formatting

--- src/App.js
+++ src/App.js
@@ -2,35 +2,27 @@ import "./App.css";
 import Name from "./components/Name";
 import Contact from "./components/Contact";
 import Location from "./components/Location";
-import Button from "./components/Button";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Button />
-        <div>
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Location</th>
-              <th>Contact</th>
-            </tr>
-            <tr>
-              <td>
-                <Name />
-              </td>
-              <td>
-                <Location />
-              </td>
-              <td>
-                <Contact />
-              </td>
-            </tr>
-          </table>
-        </div>
-      </div>
-    </div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Location</th>
+        <th>Contact</th>
+      </tr>
+      <tr>
+        <td>
+          <Name />
+        </td>
+        <td>
+          <Location />
+        </td>
+        <td>
+          <Contact />
+        </td>
+      </tr>
+    </table>
   );
 }
 
